ac68dcd7cc39f6f9fb2f482104bd1513a88110e8
Shanel Hickman-Davis <shanelhickmandavis@gmail.com>
2022-09-29 15:23:20 -0400

    tip calculator express application created

--- server.js
+++ server.js
@@ -5,8 +5,18 @@ const app = express()
 const PORT = 3000
 
 //greeting express application
-app.get('/greeting/:name', (req, res)) => {
+app.get('/greeting/:name', (req, res) => {
     req.params; //name: name
     console.log(req.params);
-    res.send(`<h1> Hello, {req.param.name}</h1>`)
-}
+    res.send(`<h1> Hello, ${req.param.name}</h1>`)
+})
+
+//tip calculator
+app.get(`/tip/:total/:tipPercentage`, (req, res) => {
+    req.params; //total: total tipPercentage:tipPercentage
+    console.log(req.params);
+    res.send(`${total * tipPercentage}`)
+})
+
+
+
